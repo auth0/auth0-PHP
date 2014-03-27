2ae799715ae9d9749f1a702d583af66330f3592b
root <root@ubuntu.(none)>
2014-03-27 14:02:37 -0700

    Changed include path to support new folder

--- examples/basic/callback.php
+++ examples/basic/callback.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require_once '../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 require_once 'config.php';
 
 use Auth0SDK\Auth0;
--- examples/basic/index.php
+++ examples/basic/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 require_once 'config.php';
 
 use Auth0SDK\Auth0;
