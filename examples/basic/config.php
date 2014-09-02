e53b1d9468f11db14626d8d31fa0a51dfb6fb588
Martin Gontovnikas <martin@gon.to>
2014-09-02 14:08:55 -0300

    Removed ugly basic example

--- examples/basic/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$auth0_cfg = array(
-    'domain'        => 'example.auth0.com',
-    'client_id'     => 'XXXXXXXX',
-    'client_secret' => 'XXXXXXXX',
-    'redirect_uri'  => 'http://localhost:[port]/callback.php'
-);
