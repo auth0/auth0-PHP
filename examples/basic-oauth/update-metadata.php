f6b8303b5fb08d5d6c1903df3c218a4a1b1a6e75
German Lena <german.lena@gmail.com>
2015-04-29 16:57:31 -0300

    updated example

---
+++ examples/basic-oauth/update-metadata.php
@@ -0,0 +1,23 @@
+<?php
+    
+    if (isset($_REQUEST['update']) && $_REQUEST['update']) {
+        
+        $newMetadata = json_decode($_REQUEST["metadata"], true);
+        
+        $auth0Oauth->updateUserMetadata($newMetadata);
+        
+        echo "<div>UPDATED!!!</div>";
+    }  
+  
+$userInfo = $auth0Oauth->getUserInfo();
+  
+?>
+
+<form action="?update-metadata" method="POST">
+
+    <textarea name='metadata'>
+        <?php echo json_encode($userInfo["user_metadata"]); ?>
+    </textarea>  
+    
+    <input type="submit" name="update" value="Update" />
+</form>
