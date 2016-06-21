34b3d189778be0d07138a45c202b41d3a42fd148
German Lena <german.lena@gmail.com>
2016-06-21 10:03:49 -0300

    More descriptive error message when code exchange fails #86

--- src/Auth0.php
+++ src/Auth0.php
@@ -230,15 +230,19 @@ class Auth0 {
         // Generate the url to the API that will give us the access token and id token
         $auth_url = $this->generateUrl('token');
         // Make the call
-        $auth0_response = $this->oauth_client->getAccessToken($auth_url, "authorization_code", array(
+        $response = $this->oauth_client->getAccessToken($auth_url, "authorization_code", array(
             "code" => $code,
             "redirect_uri" => $this->redirect_uri
         ), array(
             'Auth0-Client' => ApiClient::getInfoHeadersData()->build()
         ));
 
-        // Parse it
-        $auth0_response = $auth0_response['result'];
+        $auth0_response = $response['result'];
+
+        if ($response['code'] !== 200) {
+            throw new ApiException($auth0_response['error'] . ': '. $auth0_response['error_description']);
+        }
+
         $this->debugInfo(json_encode($auth0_response));
         $access_token = (isset($auth0_response['access_token']))? $auth0_response['access_token'] : false;
         $id_token = (isset($auth0_response['id_token']))? $auth0_response['id_token'] : false;
@@ -246,9 +250,11 @@ class Auth0 {
         if (!$access_token) {
             throw new ApiException('Invalid access_token - Retry login.');
         }
-        if (!$id_token) {
-            throw new ApiException('Missing JWT after code exchange. Remember to ask for openid scope.');
+
+        if (!$id_token) { // id_token is not mandatory anymore. There is no need to force openid connect
+            $this->debugInfo('Missing id_token after code exchange. Remember to ask for openid scope.');
         }
+
         // Set the access token in the oauth client for future calls to the Auth0 API
         $this->oauth_client->setAccessToken($access_token);
         $this->oauth_client->setAccessTokenType(Client::ACCESS_TOKEN_BEARER);
