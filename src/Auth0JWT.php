a2aa9d003037584fd551bf92ce139aab84e61ac8
German Lena <german.lena@gmail.com>
2016-08-30 12:31:16 -0300

    BC compatibility class for JWT decoding

--- src/Auth0JWT.php
+++ src/Auth0JWT.php
@@ -1,109 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: germanlena
- * Date: 4/23/15
- * Time: 12:24 PM
- */
 
 namespace Auth0\SDK;
 
-use Auth0\SDK\Exception\InvalidTokenException;
-use Auth0\SDK\Exception\CoreException;
-use Auth0\SDK\Exception\ApiException;
-use Firebase\JWT\JWT;
-
 /**
- * @deprecated
+ * This class provides access to Auth0 JWT decoder.
+ *
+ * @author Auth0
+ * @deprecated This class is provided to bring backward compatibility and will be soon removed. Use Auth0\SDK\JWTVerifier instead
  */
 class Auth0JWT {
 
-    public static function decode($jwt, $valid_audiences, $client_secret, array $authorized_iss = []) {
+  public static function decode($jwt, $valid_audiences, $client_secret, array $authorized_iss = [], $cache = null) {
 
-        if (!is_array($valid_audiences)) {
-            $valid_audiences = [$valid_audiences];
-        }
-        
-        $tks = explode('.', $jwt);
-        if (count($tks) != 3) {
-            throw new InvalidTokenException('Wrong number of segments');
-        }
-        $headb64 = $tks[0];
-        $body64 = $tks[1];
-        $head = json_decode(JWT::urlsafeB64Decode($headb64));
+    $verifier = new JWTVerifier([
+        'valid_audiences' => is_array($valid_audiences) ? $valid_audiences : [$valid_audiences],
+        'client_secret' => $client_secret,
+        'authorized_iss' => $authorized_iss,
+        'cache' => $cache,
+    ]);
 
-        if ( !is_object($head) || ! isset($head->alg))
-        {
-            throw new InvalidTokenException("Invalid token");
-        }
-
-        if ($head->alg === 'RS256') {
-            $body = json_decode(JWT::urlsafeB64Decode($body64));
-            if ( !in_array($body->iss, $authorized_iss) ) {
-                throw new CoreException("We can't trust on a token issued by: `{$body->iss}`.");
-            }
-            $secret = self::fetch_public_key($body->iss);
-        } elseif ($head->alg === 'HS256') {
-            $secret = JWT::urlsafeB64Decode($client_secret);
-        } else {
-            throw new CoreException("Invalid signature algorithm");
-        }
-        
-        try {
-            // Decode the user
-            $decodedToken = JWT::decode($jwt, $secret, array('HS256', 'RS256'));
-            // validate that this JWT was made for us
-            $audience = $decodedToken->aud;
-            if (! is_array($audience)) {
-                $audience = [$audience];
-            }
-            if (count(array_intersect($audience, $valid_audiences)) == 0) {
-                throw new CoreException("This token is not intended for us.");
-            }
-        } catch(\Exception $e) {
-            throw new CoreException($e->getMessage());
-        }
-        return $decodedToken;
-    }
-
-    /**
-     * $scopes: should be an array with the follow structure:
-     *
-     *          'scope' => [
-     *              'actions' => ['action1', 'action2']
-     *          ],
-     *          'scope2' => [
-     *              'actions' => ['action1', 'action2']
-     *          ]
-     */
-    public static function encode($audience, $client_secret, $scopes = null, $custom_payload = null, $lifetime = 36000) {
-
-        $time = time();
-
-        $payload = array(
-            "iat" => $time,
-        );
-
-        if ($scopes) {
-            $payload["scopes"] = $scopes;
-        }
-
-        if ($custom_payload) {
-            $payload = array_merge($custom_payload, $payload);
-        }
-
-        $jti = md5(json_encode($payload));
-
-        $payload['jti'] = $jti;
-        $payload["exp"] = $time + $lifetime;
-        $payload["aud"] = $audience;
-
-        $secret = base64_decode(strtr($client_secret, '-_', '+/'));
-
-        $jwt = JWT::encode($payload, $secret);
-
-        return $jwt;
-
-    }
-
+    return $verifier->verifyAndDecode($jwt);
+  }
 }
--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -33,4 +33,27 @@ class TokenTest extends \PHPUnit_Framework_TestCase {
         $this->assertArraySubset(['read'], $decoded->scopes->users->actions);
     }
 
+    public function deprecatedTestTokenGenerationDecode() {
+
+        $client_id = 'client_id_1';
+        $client_secret = 'client_secret_1';
+        
+        $generator = new TokenGenerator([ 'client_id' => $client_id, 'client_secret' => $client_secret]);
+        
+        $jwt = $generator->generate([
+            'users' => [
+                'actions' => ['read']
+            ]
+        ]);
+
+        $decoded = Auth0JWT::decode($jwt, $client_id, $client_secret);
+
+        $this->assertObjectHasAttribute('aud', $decoded);
+        $this->assertEquals($client_id, $decoded->aud);
+        $this->assertObjectHasAttribute('scopes', $decoded);
+        $this->assertObjectHasAttribute('users', $decoded->scopes);
+        $this->assertObjectHasAttribute('actions', $decoded->scopes->users);
+        $this->assertArraySubset(['read'], $decoded->scopes->users->actions);
+    }
+
 }
