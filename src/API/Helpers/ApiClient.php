95146fb335cabd849c0c3ca08d3be6b0c0faa7a8
German Lena <german.lena@gmail.com>
2016-10-27 11:47:54 -0300

    bump

--- src/API/Helpers/ApiClient.php
+++ src/API/Helpers/ApiClient.php
@@ -12,7 +12,7 @@ use Auth0\SDK\API\Header\Header;
 
 class ApiClient {
 
-    const API_VERSION  = "4.0.3";
+    const API_VERSION  = "4.0.5";
 
     protected static $infoHeadersDataEnabled = true;
     protected static $infoHeadersData;
