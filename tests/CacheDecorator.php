7b744eba0ae302d37bca0024cc3ea8521f4584a4
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-03 12:34:20 +0200

    Added support for PSR simple cache adapters (#172)
    * Removed our cache layer
    
    * Removed PHP5.5

--- tests/CacheDecorator.php
+++ tests/CacheDecorator.php
@@ -1,47 +1,89 @@
 <?php
 
-namespace Auth0\Tests;
+namespace Auth0\tests;
 
-use Auth0\SDK\Helpers\Cache\CacheHandler;
+use Psr\SimpleCache\CacheInterface;
 
-class CacheDecorator implements CacheHandler
+class CacheDecorator implements CacheInterface
 {
-  
-  protected $cache;
-  protected $counter = [];
+    private $cache;
+    private $counter = [];
 
-  public function __construct(CacheHandler $cache) 
-  {
-    $this->cache = $cache;
-  }
-
-  public function get($key) {
-    $this->add_count('get');
-    return $this->cache->get($key);
-  }
-
-  public function delete($key) {
-    $this->add_count('delete');
-    return $this->cache->delete($key);
-  }
-  
-  public function set($key, $value) {
-    $this->add_count('set');
-    return $this->cache->set($key, $value);
-  }
-
-  private function add_count($method) {
-    if (!isset($this->counter[$method])) {
-      $this->counter[$method] = 0;
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
     }
-    $this->counter[$method]++;
-  }
 
-  public function count($method) {
-    if (!isset($this->counter[$method])) {
-      return null;
+    public function get($key, $default = null)
+    {
+        $this->add_count('get');
+
+        return $this->cache->get($key, $default);
     }
-    return $this->counter[$method];
-  }
 
+    public function set($key, $value, $ttl = null)
+    {
+        $this->add_count('set');
+
+        return $this->cache->set($key, $value, $ttl);
+    }
+
+    public function delete($key)
+    {
+        $this->add_count('delete');
+
+        return $this->cache->delete($key);
+    }
+
+    public function clear()
+    {
+        $this->add_count('clear');
+
+        return $this->cache->clear();
+    }
+
+    public function getMultiple($keys, $default = null)
+    {
+        $this->add_count('getMultiple');
+
+        return $this->cache->getMultiple($keys, $default);
+    }
+
+    public function setMultiple($values, $ttl = null)
+    {
+        $this->add_count('setMultiple');
+
+        return $this->cache->setMultiple($values, $ttl);
+    }
+
+    public function deleteMultiple($keys)
+    {
+        $this->add_count('deleteMultiple');
+
+        return $this->cache->deleteMultiple($keys);
+    }
+
+    public function has($key)
+    {
+        $this->add_count('has');
+
+        return $this->cache->has($key);
+    }
+
+    private function add_count($method)
+    {
+        if (!isset($this->counter[$method])) {
+            $this->counter[$method] = 0;
+        }
+        ++$this->counter[$method];
+    }
+
+    public function count($method)
+    {
+        if (!isset($this->counter[$method])) {
+            return null;
+        }
+
+        return $this->counter[$method];
+    }
 }
