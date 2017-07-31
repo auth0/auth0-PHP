62ddd953bd95cbd52a06ac4c5b056804c0babc4c
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-31 12:59:06 +0200

    Added example test

---
+++ tests/API/Management/BaseManagementTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace auth0\tests\API\Management;
+
+use Auth0\SDK\API\Management;
+use Auth0\Tests\API\ApiTests;
+use GuzzleHttp\Psr7\Response;
+use Http\Client\Common\HttpMethodsClient;
+use Http\Client\HttpClient;
+use Http\Discovery\MessageFactoryDiscovery;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+abstract class BaseManagementTest extends ApiTests
+{
+    /**
+     * @param HttpClient $httpClient
+     *
+     * @return Management
+     */
+    protected function getManagementApi($httpClient)
+    {
+        return new Management(
+            'token', 'domain.com', new HttpMethodsClient($httpClient, MessageFactoryDiscovery::find())
+        );
+    }
+
+    /**
+     * @return Response
+     */
+    protected function createResponse($body = null, $httpStatus = 200, $headers = [])
+    {
+        if (!isset($headers['Content-Type'])){
+            $headers['Content-Type'] = 'application/json';
+        }
+        return new Response($httpStatus, $headers, $body);
+    }
+}
