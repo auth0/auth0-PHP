994a8d1046ca105a72aca6e60de0383535667023
Josh Cunningham <josh@joshcanhelp.com>
2018-05-30 17:07:03 -0700

    Clients endpoint pagination and improvements
    Add pagination to getAll method via page and per_page parameters. Add
    application type to getAll method via app_type parameter and app_type
    constants. Change apiClient fluent methods to explicit. Add
    validation for required 'name' parameter in create method. Add
    docblocks throughout Clients. Add additional testing for Clients
    and BasicCrudTest.

--- src/API/Management/Clients.php
+++ src/API/Management/Clients.php
@@ -1,33 +1,95 @@
 <?php
-
+/**
+ * Clients endpoints for the Management API.
+ *
+ * @package Auth0\SDK\API\Management
+ */
 namespace Auth0\SDK\API\Management;
 
-
-use Auth0\SDK\API\Header\ContentType;
-
-class Clients extends GenericResource 
+/**
+ * Class Clients.
+ * Handles requests to the Clients endpoint of the v2 Management API.
+ *
+ * @package Auth0\SDK\API\Management
+ */
+class Clients extends GenericResource
 {
     /**
-     * @param null|string|array $fields
-     * @param null|string|array $include_fields
-     * @return mixed
+     * Allowed app types for self::getAll().
      */
-    public function getAll($fields = null, $include_fields = null) 
-    {
-        $request = $this->apiClient->get()
-            ->clients();
+    const APP_TYPE_NATIVE = 'native';
+    const APP_TYPE_NON_INTERACTIVE = 'non_interactive';
+    const APP_TYPE_REGULAR_WEB = 'regular_web';
+    const APP_TYPE_SPA = 'spa';
 
-        if ($fields !== null) 
-        {
-            if (is_array($fields)) 
-            {
+    /**
+     * Get all Clients by page.
+     *
+     * @param null|string|array $fields         - Fields to include or exclude from the result.
+     * @param null|boolean      $include_fields - True to include $fields, false to exclude $fields.
+     * @param null|string       $app_type       - Application type to get, see class constants above.
+     * @param integer           $page           - Page number to get, zero-based.
+     * @param null|integer      $per_page       - Number of results to get, null to return the default number.
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     *
+     * @link https://auth0.com/docs/api/management/v2#!/Clients/get_clients
+     */
+    public function getAll($fields = null, $include_fields = null, $app_type = null, $page = 0, $per_page = null)
+    {
+        $request = $this->apiClient->method('get')->addPath('clients');
+
+        if (!empty($fields)) {
+            if (is_array($fields)) {
                 $fields = implode(',', $fields);
             }
             $request->withParam('fields', $fields);
         }
 
-        if ($include_fields !== null) 
-        {
+        if (null !== $include_fields) {
+            $request->withParam('include_fields', $include_fields);
+        }
+
+        if (null !== $app_type) {
+            $request->withParam('app_type', $app_type);
+        }
+
+        $request->withParam('page', abs(intval($page)));
+
+        if (null !== $per_page) {
+            $request->withParam('per_page', $per_page);
+        }
+
+        return $request->call();
+    }
+
+    /**
+     * Get a single Client by ID.
+     *
+     * @param string            $id             - Client ID to get.
+     * @param null|string|array $fields         - Fields to include or exclude, based on $include_fields parameter.
+     * @param null|string|array $include_fields - Should the field(s) above be included or excluded?
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     *
+     * @link https://auth0.com/docs/api/management/v2#!/Clients/get_clients_by_id
+     */
+    public function get($id, $fields = null, $include_fields = null)
+    {
+        $request = $this->apiClient->method('get')->addPath('clients', $id);
+
+        if (!empty($fields)) {
+            if (is_array($fields)) {
+                $fields = implode(',', $fields);
+            }
+            $request->withParam('fields', $fields);
+        }
+
+        if (null !== $include_fields) {
             $request->withParam('include_fields', $include_fields);
         }
 
@@ -35,66 +97,62 @@ class Clients extends GenericResource
     }
 
     /**
-     * @param string $id
-     * @param null|string|array $fields
-     * @param null|string|array $include_fields
-     * @return mixed
+     * Delete a client.
+     *
+     * @param string $id - Client ID to delete.
+     *
+     * @return mixed|string
+     *
+     * @throws \Exception
+     *
+     * @link https://auth0.com/docs/api/management/v2#!/Clients/delete_clients_by_id
      */
-    public function get($id, $fields = null, $include_fields = null) 
+    public function delete($id)
     {
-        $request = $this->apiClient->get()
-            ->clients($id);
-
-        if ($fields !== null) 
-        {
-            if (is_array($fields)) 
-            {
-                $fields = implode(',', $fields);
-            }
-            $request->withParam('fields', $fields);
-        }
-        if ($include_fields !== null) 
-        {
-            $request->withParam('include_fields', $include_fields);
-        }
-
-        return $request->call();
-    }
-
-    /**
-     * @param string $id
-     * @return mixed
-     */
-    public function delete($id) 
-    {
-        return $this->apiClient->delete()
-            ->clients($id)
+        return $this->apiClient->method('delete')
+            ->addPath('clients', $id)
             ->call();
     }
 
     /**
-     * @param array $data
-     * @return mixed
+     * Create a new Client.
+     *
+     * @param array $data - Client create data.
+     *
+     * @return mixed|string
+     *
+     * @throws \Exception
+     *
+     * @link https://auth0.com/docs/api/management/v2#!/Clients/post_clients
      */
-    public function create($data) 
+    public function create($data)
     {
-        return $this->apiClient->post()
-            ->clients()
-            ->withHeader(new ContentType('application/json'))
+        if (empty($data['name'])) {
+            throw new \Exception('Missing required "name" field.');
+        }
+
+        return $this->apiClient->method('post')
+            ->addPath('clients')
             ->withBody(json_encode($data))
             ->call();
     }
 
     /**
-     * @param string $id
-     * @param array $data
-     * @return mixed
+     * Update a Client.
+     *
+     * @param string $id - Client ID to update
+     * @param array $data - Client data to update.
+     *
+     * @return mixed|string
+     *
+     * @throws \Exception
+     *
+     * @link https://auth0.com/docs/api/management/v2#!/Clients/patch_clients_by_id
      */
-    public function update($id, $data) 
+    public function update($id, $data)
     {
-        return $this->apiClient->patch()
-            ->clients($id)
-            ->withHeader(new ContentType('application/json'))
+        return $this->apiClient->method('patch')
+            ->addPath('clients', $id)
             ->withBody(json_encode($data))
             ->call();
     }
--- tests/API/Management/ClientsTest.php
+++ tests/API/Management/ClientsTest.php
@@ -2,42 +2,197 @@
 namespace Auth0\Tests\API\Management;
 
 use Auth0\SDK\API\Management;
+use Auth0\SDK\API\Management\Clients;
 use Auth0\Tests\API\BasicCrudTest;
 
-class ClientsTest extends BasicCrudTest {
-    
-    protected function getId($entity) {
-        return $entity['client_id'];
+/**
+ * Class ClientsTest
+ *
+ * @package Auth0\Tests\API\Management
+ */
+class ClientsTest extends BasicCrudTest
+{
+    /**
+     * Unique identifier name for Clients.
+     *
+     * @var string
+     */
+    protected $id_name = 'client_id';
+
+    /**
+     * Name of the created Client.
+     * Appended with a random string in self::__construct().
+     *
+     * @var string
+     */
+    protected $create_client_name = 'TEST-CREATE-CLIENT-';
+
+    /**
+     * Application type of the created Client.
+     *
+     * @var string
+     */
+    protected $create_app_type = Clients::APP_TYPE_REGULAR_WEB;
+
+    /**
+     * SSO setting of the created Client.
+     *
+     * @var bool
+     */
+    protected $create_sso = false;
+
+    /**
+     * Description of the created Client.
+     *
+     * @var string
+     */
+    protected $create_desc = '__Auth0_PHP_initial_app_description__';
+
+    /**
+     * Name of the updated Client.
+     * Appended with a random string in self::__construct().
+     *
+     * @var string
+     */
+    protected $update_client_name = 'TEST-UPDATE-CLIENT-';
+
+    /**
+     * Application type of the updated Client.
+     *
+     * @var string
+     */
+    protected $update_app_type = Clients::APP_TYPE_NON_INTERACTIVE;
+
+    /**
+     * SSO setting of the updated Client.
+     *
+     * @var bool
+     */
+    protected $update_sso = true;
+
+    /**
+     * Description of the updated Client.
+     *
+     * @var string
+     */
+    protected $update_desc = '__Auth0_PHP_updated_app_description__';
+
+    /**
+     * ClientsTest constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->create_client_name .= rand();
+        $this->update_client_name .= rand();
     }
 
-    protected function getApiClient() {
-        $env = $this->getEnv();
-        $token = $this->getToken($env, [
-            'clients' => [
-                'actions' => ['create', 'read', 'delete', 'update']
-            ]
-        ]);
+    /**
+     * Return the ID for the entity created.
+     *
+     * @param array $entity - Client created during test.
+     *
+     * @return string|integer
+     */
+    protected function getId($entity)
+    {
+        return $entity[$this->id_name];
+    }
 
-        $this->domain = $env['DOMAIN'];
-
-        $api = new Management($token, $env['DOMAIN']);
-
+    /**
+     * Return the Clients API to test.
+     *
+     * @return Management\Clients
+     */
+    protected function getApiClient()
+    {
+        $token = $this->getToken($this->env, [ 'clients' => [ 'actions' => ['create', 'read', 'delete', 'update' ] ] ]);
+        $api = new Management($token, $this->domain);
         return $api->clients;
     }
 
-    protected function getCreateBody() {
-        $client_name = 'test-create-client' . rand();
-        echo "\n-- Using client name $client_name \n";
+    /**
+     * Get the Client create data to send with the test create call.
+     *
+     * @return array
+     */
+    protected function getCreateBody()
+    {
+        return [
+            'name' => $this->create_client_name,
+            'app_type' => $this->create_app_type,
+            'sso' => $this->create_sso,
+            'description' => $this->create_desc,
+        ];
+    }
 
-        return ['name' => $client_name, 'sso' => false];
+    /**
+     * Tests the \Auth0\SDK\API\Management\Clients::getAll() method.
+     *
+     * @param Management\Clients $client - API client to use.
+     * @param array $created_entity - Entity created during create() test.
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     */
+    protected function getAll($client, $created_entity)
+    {
+        $fields = array_keys($this->getCreateBody());
+        $fields[] = $this->id_name;
+
+        // Check that pagination works.
+        $all_results = $client->getAll($fields, true, $this->create_app_type, 1, 1);
+        $this->assertEquals(1, count($all_results));
+        $this->assertEquals(count($fields), count($all_results[0]));
+
+        // If we want to check for the created result, we need all Clients.
+        if ($this->findCreatedItem) {
+            $all_results = $client->getAll($fields, true, $this->create_app_type, 0, 100);
+        }
+
+        return $all_results;
     }
-    protected function getUpdateBody() {
-        return ['sso' => true];
+
+    /**
+     * Check that the Client created matches the initial values sent.
+     *
+     * @param array $entity - The created Client to check against initial values.
+     */
+    protected function afterCreate($entity)
+    {
+        $this->assertNotEmpty($entity[$this->id_name]);
+        $this->assertEquals($this->create_client_name, $entity['name']);
+        $this->assertEquals($this->create_app_type, $entity['app_type']);
+        $this->assertEquals($this->create_sso, $entity['sso']);
+        $this->assertEquals($this->create_desc, $entity['description']);
     }
-    protected function afterCreate($entity) {
-        $this->assertNotTrue($entity['sso']);
+
+    /**
+     * Get the Client values that should be updated.
+     *
+     * @return array
+     */
+    protected function getUpdateBody()
+    {
+        return [
+            'name' => $this->update_client_name,
+            'app_type' => $this->update_app_type,
+            'sso' => $this->update_sso,
+            'description' => $this->update_desc,
+        ];
     }
-    protected function afterUpdate($entity) {
-        $this->assertTrue($entity['sso']);
+
+    /**
+     * Update entity returned values check.
+     *
+     * @param array $entity - Client that was updated.
+     */
+    protected function afterUpdate($entity)
+    {
+        $this->assertEquals($this->update_client_name, $entity['name']);
+        $this->assertEquals($this->update_app_type, $entity['app_type']);
+        $this->assertEquals($this->update_sso, $entity['sso']);
+        $this->assertEquals($this->update_desc, $entity['description']);
     }
 }
