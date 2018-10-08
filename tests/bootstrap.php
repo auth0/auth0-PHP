e155c56719691f7c964d32cfcc1260a84cfe24cc
Josh Cunningham <josh@joshcanhelp.com>
2018-10-08 10:00:56 -0700

    Explicitly require ErrorHelper trait

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,5 @@ require_once $tests_dir.'../vendor/autoload.php';
 if (! defined( 'AUTH0_PHP_TEST_JSON_DIR' )) {
     define( 'AUTH0_PHP_TEST_JSON_DIR', $tests_dir.'json/' );
 }
+
+require_once $tests_dir.'traits/ErrorHelpers.php';
