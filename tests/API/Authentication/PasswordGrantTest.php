d569145bbddef61e9207e5f6395b5568f1c462c6
Josh Cunningham <josh@joshcanhelp.com>
2019-02-14 10:17:39 -0800

    Add test coverage for login method (realm)

--- tests/API/Authentication/PasswordGrantTest.php
+++ tests/API/Authentication/PasswordGrantTest.php
@@ -52,6 +52,15 @@ class PasswordGrantTest extends ApiTests
         }
 
         $this->assertTrue( $caught_exception );
+
+        try {
+            $caught_exception = false;
+            $api->login( [] );
+        } catch (ApiException $e) {
+            $caught_exception = $this->errorHasString( $e, 'username is mandatory' );
+        }
+
+        $this->assertTrue( $caught_exception );
     }
 
     public function testThatPasswordGrantLoginEnforcesPassword()
@@ -63,6 +72,29 @@ class PasswordGrantTest extends ApiTests
             $api->login_with_default_directory( [ 'username' => uniqid() ]);
         } catch (ApiException $e) {
             $caught_exception = $this->errorHasString( $e, 'password is mandatory' );
+        }
+
+        $this->assertTrue( $caught_exception );
+
+        try {
+            $caught_exception = false;
+            $api->login( [ 'username' => uniqid() ]);
+        } catch (ApiException $e) {
+            $caught_exception = $this->errorHasString( $e, 'password is mandatory' );
+        }
+
+        $this->assertTrue( $caught_exception );
+    }
+
+    public function testThatPasswordGrantRealmLoginEnforcesRealm()
+    {
+        $api = new Authentication( 'test-domain.auth0.com' );
+
+        try {
+            $caught_exception = false;
+            $api->login( [ 'username' => uniqid(), 'password' => uniqid() ]);
+        } catch (ApiException $e) {
+            $caught_exception = $this->errorHasString( $e, 'realm is mandatory' );
         }
 
         $this->assertTrue( $caught_exception );
@@ -79,7 +111,7 @@ class PasswordGrantTest extends ApiTests
 
         $api->call()->login_with_default_directory( [
             'username' => 'the_username',
-            'password' => 'the_password'
+            'password' => 'the_password',
         ] );
 
         $this->assertEquals( 'https://test-domain.auth0.com/oauth/token', $api->getHistoryUrl() );
@@ -96,6 +128,28 @@ class PasswordGrantTest extends ApiTests
         $this->assertEquals( 'password', $request_body['grant_type'] );
         $this->assertEquals( '__test_client_id__', $request_body['client_id'] );
         $this->assertEquals( '__test_client_secret__', $request_body['client_secret'] );
+    }
+
+    /**
+     * Test that a basic password grant realm request includes the realm.
+     *
+     * @throws ApiException If username or password is missing.
+     */
+    public function testThatPasswordGrantRealmLoginSendsBasicRequestCorrectly()
+    {
+        $api = new MockAuthenticationApi([new Response(200)]);
+
+        $api->call()->login( [
+            'username' => 'the_username',
+            'password' => 'the_password',
+            'realm'    => 'the_realm',
+        ] );
+
+        $this->assertEquals( 'https://test-domain.auth0.com/oauth/token', $api->getHistoryUrl() );
+
+        $request_body = $api->getHistoryBody();
+        $this->assertEquals( 'the_realm', $request_body['realm'] );
+        $this->assertEquals( 'http://auth0.com/oauth/grant-type/password-realm', $request_body['grant_type'] );
     }
 
     /**
@@ -128,5 +182,39 @@ class PasswordGrantTest extends ApiTests
         $request_headers = $api->getHistoryHeaders();
         $this->assertArrayHasKey( 'Auth0-Forwarded-For', $request_headers );
         $this->assertEquals( '1.2.3.4', $request_headers['Auth0-Forwarded-For'][0] );
+    }
+
+    /**
+     * Test that a password grant request including an IP address sets the correct header.
+     *
+     * @throws ApiException If username or password is missing.
+     */
+    public function testThatPasswordGrantRealmLoginSetsForwardedForHeader()
+    {
+        $api = new MockAuthenticationApi([new Response(200), new Response(200)]);
+
+        $api->call()->login(
+            [
+                'username' => uniqid(),
+                'password' => uniqid(),
+                'realm' => uniqid(),
+            ],
+            '5.6.7.8'
+        );
+
+        $request_headers = $api->getHistoryHeaders();
+        $this->assertArrayHasKey( 'Auth0-Forwarded-For', $request_headers );
+        $this->assertEquals( '5.6.7.8', $request_headers['Auth0-Forwarded-For'][0] );
+
+        $api->call()->login( [
+            'username' => uniqid(),
+            'password' => uniqid(),
+            'realm' => uniqid(),
+            'auth0_forwarded_for' => '5.6.7.8'
+        ] );
+
+        $request_headers = $api->getHistoryHeaders();
+        $this->assertArrayHasKey( 'Auth0-Forwarded-For', $request_headers );
+        $this->assertEquals( '5.6.7.8', $request_headers['Auth0-Forwarded-For'][0] );
     }
 }
