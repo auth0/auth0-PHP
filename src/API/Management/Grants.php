ffbb272462100abcd1886db63a85c5e86568f758
Josh Cunningham <josh@joshcanhelp.com>
2019-02-20 07:38:06 -0800

    Final tests, formatting, and docs

--- src/API/Management/Grants.php
+++ src/API/Management/Grants.php
@@ -3,17 +3,24 @@ namespace Auth0\SDK\API\Management;
 
 use Auth0\SDK\Exception\CoreException;
 
+/**
+ * Class Grants
+ *
+ * @package Auth0\SDK\API\Management
+ */
 class Grants extends GenericResource
 {
     /**
      * Get all Grants by page.
      * Required scope: "read:grants"
      *
-     * @param  integer      $page     The page number to return, zero-based.
-     * @param  null|integer $per_page The number of results per page, null to return all.
-     * @param  array        $params
-     * @return mixed|string
-     * @throws \Exception
+     * @param integer      $page     Page number to return, zero-based.
+     * @param null|integer $per_page Number of results per page, null to return all.
+     * @param array        $params   Additional URL parameters to send.
+     *
+     * @return mixed
+     *
+     * @throws \Exception Thrown by the HTTP client when there is a problem with the API call.
      */
     public function getAll($page = 0, $per_page = null, array $params = [])
     {
@@ -32,14 +39,19 @@ class Grants extends GenericResource
     }
 
     /**
-     * @param  $client_id
-     * @param  null      $page
-     * @param  null      $per_page
-     * @return mixed|string
-     * @throws CoreException
-     * @throws \Exception
+     * Get Grants by Client ID.
+     * Required scope: "read:grants"
+     *
+     * @param string       $client_id Client ID to filter Grants.
+     * @param integer      $page      Page number to return, zero-based.
+     * @param null|integer $per_page  Number of results per page, null to return all.
+     *
+     * @return mixed
+     *
+     * @throws CoreException If $client_id is empty or not a string.
+     * @throws \Exception Thrown by the HTTP client when there is a problem with the API call.
      */
-    public function getByClientId($client_id, $page = null, $per_page = null)
+    public function getByClientId($client_id, $page = 0, $per_page = null)
     {
         if (empty($client_id) || ! is_string($client_id)) {
             throw new CoreException('Empty or invalid "client_id" parameter.');
@@ -49,12 +61,17 @@ class Grants extends GenericResource
     }
 
     /**
-     * @param  $audience
-     * @param  null     $page
-     * @param  null     $per_page
-     * @return mixed|string
-     * @throws CoreException
-     * @throws \Exception
+     * Get Grants by Audience.
+     * Required scope: "read:grants"
+     *
+     * @param string       $audience Audience to filter Grants.
+     * @param integer      $page     Page number to return, zero-based.
+     * @param null|integer $per_page Number of results per page, null to return all.
+     *
+     * @return mixed
+     *
+     * @throws CoreException If $audience is empty or not a string.
+     * @throws \Exception Thrown by the HTTP client when there is a problem with the API call.
      */
     public function getByAudience($audience, $page = null, $per_page = null)
     {
@@ -66,14 +83,19 @@ class Grants extends GenericResource
     }
 
     /**
-     * @param  $user_id
-     * @param  null    $page
-     * @param  null    $per_page
-     * @return mixed|string
-     * @throws CoreException
-     * @throws \Exception
+     * Get Grants by User ID.
+     * Required scope: "read:grants"
+     *
+     * @param string       $user_id  User ID to filter Grants.
+     * @param integer      $page     Page number to return, zero-based.
+     * @param null|integer $per_page Number of results per page, null to return all.
+     *
+     * @return mixed
+     *
+     * @throws CoreException If $user_id is empty or not a string.
+     * @throws \Exception Thrown by the HTTP client when there is a problem with the API call.
      */
-    public function getByUserId($user_id, $page = null, $per_page = null)
+    public function getByUserId($user_id, $page = 0, $per_page = null)
     {
         if (empty($user_id) || ! is_string($user_id)) {
             throw new CoreException('Empty or invalid "user_id" parameter.');
@@ -83,9 +105,15 @@ class Grants extends GenericResource
     }
 
     /**
-     * @param  $id
-     * @return mixed|string
-     * @throws \Exception
+     * Delete a grant by Grant ID or User ID.
+     * Required scope: "delete:grants"
+     *
+     * @param string $id Grant ID to delete a single Grant or User ID to delete all Grants for a User.
+     *
+     * @return mixed
+     *
+     * @throws CoreException If $id is empty or not a string.
+     * @throws \Exception Thrown by the HTTP client when there is a problem with the API call.
      */
     public function delete($id)
     {
@@ -96,15 +124,5 @@ class Grants extends GenericResource
         return $this->apiClient->method('delete')
             ->addPath('grants', $id)
             ->call();
-    }
-
-    /**
-     * @param  $user_id
-     * @return mixed|string
-     * @throws \Exception
-     */
-    public function deleteUserGrants($user_id)
-    {
-        return $this->delete($user_id);
     }
 }
