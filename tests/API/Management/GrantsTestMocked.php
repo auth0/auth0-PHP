cfa823cde7eea4d5a92e3afea527b6fa96ae8819
Josh Cunningham <josh@joshcanhelp.com>
2019-02-20 07:38:06 -0800

    Final tests, formatting, and docs

--- tests/API/Management/GrantsTestMocked.php
+++ tests/API/Management/GrantsTestMocked.php
@@ -1,10 +1,11 @@
 <?php
 namespace Auth0\Tests\API\Management;
 
-use Auth0\Tests\MockApi;
-use Auth0\Tests\Traits\ErrorHelpers;
-
+use Auth0\SDK\API\Management;
 use Auth0\SDK\API\Helpers\InformationHeaders;
+use Auth0\SDK\Exception\CoreException;
+
+use Auth0\Tests\Traits\ErrorHelpers;
 
 use GuzzleHttp\Psr7\Response;
 
@@ -36,7 +37,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test a basic getAll grant call.
+     * Test that getAll requests properly.
      *
      * @return void
      *
@@ -44,7 +45,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
      */
     public function testGrantsGetAll()
     {
-        $api = new MockApi( [ new Response( 200 ) ] );
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
 
         $api->call()->grants->getAll();
 
@@ -58,7 +59,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test a paginated getAll request.
+     * Test that getAll sends pagination parameters with the request.
      *
      * @return void
      *
@@ -66,7 +67,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
      */
     public function testThatGrantsGetAllPaginates()
     {
-        $api = new MockApi( [ new Response( 200 ), new Response( 200 ), new Response( 200 ) ] );
+        $api = new MockManagementApi( [ new Response( 200 ), new Response( 200 ), new Response( 200 ) ] );
 
         $page     = 1;
         $per_page = 5;
@@ -92,7 +93,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test a getAll request with additional parameters added.
+     * Test that getAll sends additional parameters with the request.
      *
      * @return void
      *
@@ -100,7 +101,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
      */
     public function testThatGrantsGetAllAddsExtraParams()
     {
-        $api = new MockApi( [ new Response( 200 ) ] );
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
 
         $add_params = ['param1' => 'value1', 'param2' => 'value2'];
         $api->call()->grants->getAll(null, null, $add_params);
@@ -112,7 +113,172 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test a basic delete grant request.
+     * Test that getByClientId adds a client_id to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByClientId()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByClientId('__test_client_id__');
+
+        $this->assertStringStartsWith( 'https://api.test.local/api/v2/grants', $api->getHistoryUrl() );
+        $this->assertEquals( 'client_id=__test_client_id__', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByClientId adds pagination parameters to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByClientIdPaginates()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByClientId('__test_client_id__', 1, 2);
+
+        $this->assertContains( 'page=1', $api->getHistoryQuery() );
+        $this->assertContains( 'per_page=2', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByClientId throws an exception with an empty parameter.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByClientIdThrowsException()
+    {
+        $api = new Management( '__test_api_token__', '__test_domain__' );
+
+        try {
+            $caught_exception = false;
+            $api->grants->getByClientId( '' );
+        } catch (CoreException $e) {
+            $caught_exception = $this->errorHasString( $e, 'Empty or invalid "client_id" parameter' );
+        }
+
+        $this->assertTrue( $caught_exception );
+    }
+
+    /**
+     * Test that getByAudience adds an audience to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByAudience()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByAudience('__test_audience__');
+
+        $this->assertStringStartsWith( 'https://api.test.local/api/v2/grants', $api->getHistoryUrl() );
+        $this->assertEquals( 'audience=__test_audience__', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByAudience adds pagination parameters to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByAudiencePaginates()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByAudience('__test_audience__', 1, 2);
+
+        $this->assertContains( 'page=1', $api->getHistoryQuery() );
+        $this->assertContains( 'per_page=2', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByAudience throws an exception with an empty parameter.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByAudienceThrowsException()
+    {
+        $api = new Management( '__test_api_token__', '__test_domain__' );
+
+        try {
+            $caught_exception = false;
+            $api->grants->getByAudience( '' );
+        } catch (CoreException $e) {
+            $caught_exception = $this->errorHasString( $e, 'Empty or invalid "audience" parameter' );
+        }
+
+        $this->assertTrue( $caught_exception );
+    }
+
+    /**
+     * Test that getByUserId adds an audience to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByUserId()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByUserId('__test_user_id__');
+
+        $this->assertStringStartsWith( 'https://api.test.local/api/v2/grants', $api->getHistoryUrl() );
+        $this->assertEquals( 'user_id=__test_user_id__', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByUserId adds pagination parameters to the request.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByUserIdPaginates()
+    {
+        $api = new MockManagementApi( [ new Response( 200 ) ] );
+
+        $api->call()->grants->getByUserId('__test_user_id__', 1, 2);
+
+        $this->assertContains( 'page=1', $api->getHistoryQuery() );
+        $this->assertContains( 'per_page=2', $api->getHistoryQuery() );
+    }
+
+    /**
+     * Test that getByUserId throws an exception with an empty parameter.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testThatGrantsGetByUserIdThrowsException()
+    {
+        $api = new Management( '__test_api_token__', '__test_domain__' );
+
+        try {
+            $caught_exception = false;
+            $api->grants->getByUserId( '' );
+        } catch (CoreException $e) {
+            $caught_exception = $this->errorHasString( $e, 'Empty or invalid "user_id" parameter' );
+        }
+
+        $this->assertTrue( $caught_exception );
+    }
+
+    /**
+     * Test that delete requests properly.
      *
      * @return void
      *
@@ -120,7 +286,7 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
      */
     public function testGrantsDelete()
     {
-        $api = new MockApi( [ new Response( 204 ) ] );
+        $api = new MockManagementApi( [ new Response( 204 ) ] );
 
         $id = uniqid();
         $api->call()->grants->delete($id);
@@ -128,6 +294,30 @@ class GrantsTestMocked extends \PHPUnit_Framework_TestCase
         $this->assertEquals( 'DELETE', $api->getHistoryMethod() );
         $this->assertEquals( 'https://api.test.local/api/v2/grants/'.$id, $api->getHistoryUrl() );
         $this->assertEmpty( $api->getHistoryQuery() );
+
+        $headers = $api->getHistoryHeaders();
+        $this->assertEquals( 'Bearer __api_token__', $headers['Authorization'][0] );
+        $this->assertEquals( self::$telemetry, $headers['Auth0-Client'][0] );
     }
 
+    /**
+     * Test that a delete request throws an exception with an empty parameter.
+     *
+     * @return void
+     *
+     * @throws \Exception Unsuccessful HTTP call.
+     */
+    public function testGrantsDeleteThrowsException()
+    {
+        $api = new Management( '__test_api_token__', '__test_domain__' );
+
+        try {
+            $caught_exception = false;
+            $api->grants->delete( '' );
+        } catch (CoreException $e) {
+            $caught_exception = $this->errorHasString( $e, 'Empty or invalid "id" parameter' );
+        }
+
+        $this->assertTrue( $caught_exception );
+    }
 }
