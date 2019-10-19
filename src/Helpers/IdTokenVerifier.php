c76f4cf5aa4cf7b7680356a2bf01c9d22e45c2c8
Josh Cunningham <josh@joshcanhelp.com>
2019-10-18 18:33:41 -0700

    Add IdTokenVerifier

---
+++ src/Helpers/IdTokenVerifier.php
@@ -0,0 +1,208 @@
+<?php
+declare(strict_types=1);
+
+namespace Auth0\SDK\Helpers;
+
+use Auth0\SDK\Exception\InvalidTokenException;
+
+/**
+ * Class IdTokenVerifier
+ *
+ * @package Auth0\SDK\Helpers
+ */
+final class IdTokenVerifier
+{
+    const DEFAULT_LEEWAY = 60;
+
+    /**
+     * Token issuer base URL expected.
+     *
+     * @var string
+     */
+    private $issuer;
+
+    /**
+     * Token audience expected.
+     *
+     * @var string
+     */
+    private $audience;
+
+    /**
+     * Token signature verifier.
+     *
+     * @var SignatureVerifier
+     */
+    private $verifier;
+
+    /**
+     * IdTokenVerifier constructor.
+     *
+     * @param string            $issuer   Token issuer base URL expected.
+     * @param string            $audience Token audience expected.
+     * @param SignatureVerifier $verifier Token signature verifier.
+     */
+    public function __construct(string $issuer, string $audience, SignatureVerifier $verifier)
+    {
+        $this->issuer   = $issuer;
+        $this->audience = $audience;
+        $this->verifier = $verifier;
+    }
+
+    /**
+     * @param string $token
+     * @param array  $options
+     *
+     * @return \stdClass
+     *
+     * @throws InvalidTokenException
+     */
+    public function decode(string $token, array $options = []) : \stdClass
+    {
+        if (! empty($token)) {
+            throw new InvalidTokenException('ID token is required but missing');
+        }
+
+        $verifiedToken = $this->verifier->verifyAndDecode( $token );
+
+        /*
+         * Issuer checks
+         */
+        $tokenIss = $verifiedToken->getClaim('iss', false);
+        if (! $tokenIss || ! is_string($tokenIss)) {
+            throw new InvalidTokenException('Issuer (iss) claim must be a string present in the ID token');
+        }
+
+        if ($tokenIss !== $this->issuer) {
+            throw new InvalidTokenException( sprintf(
+                'Issuer (iss) claim mismatch in the ID token; expected "%s", found "%s"', $this->issuer, $tokenIss
+            ) );
+        }
+
+        /*
+         * Subject check
+         */
+        $tokenSub = $verifiedToken->getClaim('sub', false);
+        if (! $tokenSub || ! is_string($tokenSub)) {
+            throw new InvalidTokenException('Subject (sub) claim must be a string present in the ID token');
+        }
+
+        /*
+         * Audience checks
+         */
+        $tokenAud = $verifiedToken->getClaim('aud', false);
+        if (! $tokenAud || (! is_string($tokenAud) && ! is_array($tokenAud))) {
+            throw new InvalidTokenException(
+                'Audience (aud) claim must be a string or array of strings present in the ID token'
+            );
+        }
+
+        if (is_array($tokenAud) && in_array($this->audience, $tokenAud)) {
+            throw new InvalidTokenException( sprintf(
+                'Audience (aud) claim mismatch in the ID token; expected "%s" was not one of "%s"',
+                $this->audience,
+                implode(', ', $tokenAud)
+            ) );
+        } else if (is_string($tokenAud) && $tokenAud !== $this->audience) {
+            throw new InvalidTokenException( sprintf(
+                'Audience (aud) claim mismatch in the ID token; expected "%s", found "%s"', $this->audience, $tokenAud
+            ) );
+        }
+
+        /*
+         * Clock checks
+         */
+        $now    = $options['time'] ?? time();
+        $leeway = $options['leeway'] ?? self::DEFAULT_LEEWAY;
+
+        $tokenExp = $verifiedToken->getClaim('exp', false);
+        if (! $tokenExp || ! is_int($tokenExp)) {
+            throw new InvalidTokenException('Expiration Time (exp) claim must be a number present in the ID token');
+        }
+
+        $expireTime = $tokenExp + $leeway;
+        if ($now > $expireTime) {
+            throw new InvalidTokenException( sprintf(
+                'Expiration Time (exp) claim error in the ID token; current time (%d) is after expiration time (%d)',
+                $now,
+                $expireTime
+            ) );
+        }
+
+        $tokenIat = $verifiedToken->getClaim('iat', false);
+        if (! $tokenIat || ! is_int($tokenIat)) {
+            throw new InvalidTokenException('Issued At (iat) claim must be a number present in the ID token');
+        }
+
+        $issuedTime = $tokenIat - $leeway;
+        if ($now < $issuedTime) {
+            throw new InvalidTokenException( sprintf(
+                'Issued At (iat) claim error in the ID token; current time (%d) is before issued at time (%d)',
+                $now,
+                $issuedTime
+            ) );
+        }
+
+        /*
+         * Nonce check
+         */
+        if (! empty($options['nonce'])) {
+            $tokenNonce = $verifiedToken->getClaim('nonce', false);
+            if (! $tokenNonce || ! is_string($tokenNonce)) {
+                throw new InvalidTokenException('Nonce (nonce) claim must be a string present in the ID token');
+            }
+
+            if ($tokenNonce !== $options['nonce']) {
+                throw new InvalidTokenException( sprintf(
+                    'Nonce (nonce) claim mismatch in the ID token; expected "%s", found "%s"',
+                    $options['nonce'],
+                    $tokenNonce
+                ) );
+            }
+        }
+
+        /*
+         * Authorized party check
+         */
+        if (is_array($tokenAud) && count($tokenAud) > 1) {
+            $tokenAzp = $verifiedToken->getClaim('azp', false);
+            if (! $tokenAzp || ! is_string($tokenAzp)) {
+                throw new InvalidTokenException(
+                    'Authorized Party (azp) claim must be a string present in the ID token when Audience (aud) claim has multiple values'
+                );
+            }
+
+            if ($tokenAzp !== $this->audience) {
+                throw new InvalidTokenException( sprintf(
+                    'Authorized Party (azp) claim mismatch in the ID token; expected "%s", found "%s"',
+                    $this->audience,
+                    $tokenAzp
+                ) );
+            }
+        }
+
+        /*
+         * Authentication time check
+         */
+        if (! empty($options['max_age'])) {
+            $tokenAuthTime = $verifiedToken->getClaim('auth_time', false);
+            if (! $tokenAuthTime || ! is_int($tokenAuthTime)) {
+                throw new InvalidTokenException(
+                    'Authentication Time (auth_time) claim must be a number present in the ID token when Max Age (max_age) is specified'
+                );
+            }
+
+            $authValidUntil = $tokenAuthTime + $options['max_age'] + $leeway;
+
+            if ($now > $authValidUntil) {
+                throw new InvalidTokenException( sprintf(
+                    'Authentication Time (auth_time) claim in the ID token indicates that too much time has passed since the last end-user authentication. Current time (%d) is after last auth at %d',
+                    $now,
+                    $authValidUntil
+                ) );
+            }
+        }
+
+        return (object) $verifiedToken->getClaims();
+    }
+}
