5609f944945a770393af7f8c9ca1599b3577dd96
Josh Cunningham <josh@joshcanhelp.com>
2020-02-19 08:02:50 -0800

    Better docblocks

--- src/Helpers/Tokens/TokenVerifier.php
+++ src/Helpers/Tokens/TokenVerifier.php
@@ -69,19 +69,16 @@ class TokenVerifier
     }
 
     /**
-     * Verifies and decodes an JWT.
+     * Verifies and decodes a JWT.
      *
      * @param string $token   Raw JWT string.
      * @param array  $options Options to adjust the verification. Can be:
-     *      - "nonce" to check the nonce contained in the token (recommended).
-     *      - "max_age" to check the auth_time of the token.
-     *      - "time" Unix timestamp to use as the current time for exp, iat, and auth_time checks. Used for testing.
      *      - "leeway" clock tolerance in seconds for the current check only. See $leeway above for default.
      *
      * @return array
      *
      * @throws InvalidTokenException Thrown if:
-     *      - ID token is missing (expected but none provided)
+     *      - Token is missing (expected but none provided)
      *      - Signature cannot be verified
      *      - Token algorithm is not supported
      *      - Any claim-based test fails
