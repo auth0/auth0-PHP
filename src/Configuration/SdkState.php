4e7a2f1e11e00c75d71c4013c2bcd4733e2a9416
Evan Sims <evan.sims@auth0.com>
2021-06-22 20:46:51 -0400

    feat: Store access token scopes in session for convenience.

--- src/Configuration/SdkState.php
+++ src/Configuration/SdkState.php
@@ -12,22 +12,22 @@ use Auth0\SDK\Mixins\ConfigurableMixin;
  *
  * @method SdkState setIdToken(?string $idToken = null)
  * @method SdkState setAccessToken(?string $accessToken = null)
+ * @method SdkState setAccessTokenScope(?array $accessTokenScope = null)
  * @method SdkState setRefreshToken(?string $refreshToken = null)
- * @method SdkState setIdTokenDecoded(?array $idTokenDecoded = null)
  * @method SdkState setUser(?array $user = null)
  * @method SdkState setAccessTokenExpiration(?int $accessTokenExpiration = null)
  *
  * @method string|null getIdToken()
  * @method string|null getAccessToken()
+ * @method array<string,string>|null getAccessTokenScope()
  * @method string|null getRefreshToken()
- * @method array<mixed>|null getIdTokenDecoded()
  * @method array<string,array|int|string>|null getUser()
  * @method int|null getAccessTokenExpiration()
  *
  * @method bool hasIdToken()
  * @method bool hasAccessToken()
+ * @method bool hasAccessTokenScope()
  * @method bool hasRefreshToken()
- * @method bool hasIdTokenDecoded()
  * @method bool hasUser()
  * @method bool hasAccessTokenExpiration()
  */
@@ -38,20 +38,20 @@ final class SdkState implements ConfigurableContract
     /**
      * SdkState Constructor
      *
-     * @param array<mixed>|null $configuration         Optional. Pass an array of parameter keys and values to define the internal state of the SDK.
-     * @param string|null       $idToken               Optional. The id token currently in use for the session, if available.
-     * @param string|null       $accessToken           Optional. The access token currently in use for the session, if available.
-     * @param string|null       $refreshToken          Optional. The refresh token currently in use for the session, if available.
-     * @param array<mixed>|null $idTokenDecoded        Optional. An array representing the id token in it's decoded form, if available.
-     * @param array<mixed>|null $user                  Optional. An array representing the user data, if available.
-     * @param int|null          $accessTokenExpiration Optional. When the $accessToken is expected to expire, if available.
+     * @param array<mixed>|null  $configuration         Optional. Pass an array of parameter keys and values to define the internal state of the SDK.
+     * @param string|null        $idToken               Optional. The id token currently in use for the session, if available.
+     * @param string|null        $accessToken           Optional. The access token currently in use for the session, if available.
+     * @param array<string>|null $accessTokenScope      Optional. The scopes from the access token for the session, if available.
+     * @param string|null        $refreshToken          Optional. The refresh token currently in use for the session, if available.
+     * @param array<mixed>|null  $user                  Optional. An array representing the user data, if available.
+     * @param int|null           $accessTokenExpiration Optional. When the $accessToken is expected to expire, if available.
      */
     public function __construct(
         ?array $configuration = null,
         ?string $idToken = null,
         ?string $accessToken = null,
+        ?array $accessTokenScope = null,
         ?string $refreshToken = null,
-        ?array $idTokenDecoded = null,
         ?array $user = null,
         ?int $accessTokenExpiration = null
     ) {
