7d76663c5e145b242ef7799e7b2e1afc7e8a0fd6
Evan Sims <hello@evansims.com>
2021-08-03 21:42:18 -0400

    feat: Improvements to backoff logic

--- src/Utility/HttpRequest.php
+++ src/Utility/HttpRequest.php
@@ -23,6 +23,8 @@ final class HttpRequest
 {
     public const MAX_REQUEST_RETRIES = 10;
     public const MAX_REQUEST_RETRY_JITTER = 250;
+    public const MAX_REQUEST_RETRY_DELAY = 500;
+    public const MIN_REQUEST_RETRY_DELAY = 100;
     private const MICROSECONDS = 1000;
 
     /**
@@ -330,14 +332,27 @@ final class HttpRequest
                 $retries = min(self::MAX_REQUEST_RETRIES, $httpRetries);
 
                 if ($attempt < $retries) {
+                    /**
+                     * Use an exponential back-off with the formula:
+                     * max(floor, min(cap, random_between(0, jitter + (1000 * 2 ** attempt))))
+                     *
+                     * ✔ Each attempt increases base delay by (1000 * 2 ** attempt)
+                     * ✔ Randomizes jitter, adding up to MAX_REQUEST_RETRY_JITTER (250ms)
+                     * ✔ Never less than MIN_REQUEST_RETRY_DELAY (100ms)
+                     * ✔ Never more than MAX_REQUEST_RETRY_DELAY (500ms)
+                     */
                     $wait = intval(self::MICROSECONDS * pow(2, $attempt)); // Exponential delay with each subsequent request attempt.
-                    $wait = mt_rand($wait, $wait + self::MAX_REQUEST_RETRY_JITTER * self::MICROSECONDS); // Add jitter to the delay window.
-                    $wait = min(500 * self::MICROSECONDS, $wait); // Cap delay at 0.5 second.
+                    $wait = mt_rand(0, $wait + self::MAX_REQUEST_RETRY_JITTER * self::MICROSECONDS); // Add jitter to the delay window.
+                    $wait = min(self::MAX_REQUEST_RETRY_DELAY * self::MICROSECONDS, $wait); // Cap delay at MAX_REQUEST_RETRY_DELAY.
+                    $wait = max(self::MIN_REQUEST_RETRY_DELAY * self::MICROSECONDS, $wait); // Ensure delay is no less than MIN_REQUEST_RETRY_DELAY.
 
-                    // Briefly wait before attempting again.
-                    usleep($wait);
+                    // Do not apply delay if running inside unit tests.
+                    if (! defined('AUTH0_TESTS_DIR')) {
+                        // Briefly wait before attempting again.
+                        usleep($wait);
+                    }
 
-                    // Make another attempt.
+                    // Make subsequent attempt.
                     $httpResponse = $this->call();
                 }
             }
