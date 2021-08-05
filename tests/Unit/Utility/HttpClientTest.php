21e3a4259a4cebd43a8b437f7f6f033e0dcff6f2
Evan Sims <hello@evansims.com>
2021-08-05 16:10:47 -0400

    tests: Testing improvements.

--- tests/Unit/Utility/HttpClientTest.php
+++ tests/Unit/Utility/HttpClientTest.php
@@ -66,12 +66,15 @@ test('a 429 response is not retried more than the hard cap', function(): void {
 });
 
 test('an expontential backoff and jitter are being applied', function(): void {
-    $this->config->setHttpMaxRetries(5);
-    $baseWaits = [];
+    $this->config->setHttpMaxRetries(10);
+    $baseWaits = [0];
+    $baseWaitSum = 0;
 
     for ($i=0; $i < 10; $i++) {
         $this->client->mockResponse(clone $this->httpResponse429);
-        $baseWaits[] = intval(100 * pow(2, $i));
+        $baseWait = intval(100 * pow(2, $i));
+        $baseWaits[] = $baseWait;
+        $baseWaitSum = $baseWaitSum + $baseWait;
     }
 
     $response = $this->client->method('get')
@@ -81,25 +84,57 @@ test('an expontential backoff and jitter are being applied', function(): void {
     $requestCount = $this->client->getLastRequest()->getRequestCount();
     $requestDelays = $this->client->getLastRequest()->getRequestDelays();
 
-    // Final mock API response was a 429.
     $this->assertEquals(429, HttpResponse::getStatusCode($response));
-
-    // We made 5 requests.
-    $this->assertEquals(5, $requestCount);
-
-    // We triggered usleep() 4 times.
-    $this->assertEquals(4, count($requestDelays));
+    $this->assertEquals(10, $requestCount);
+    $this->assertEquals(10, count($requestDelays));
 
     // Assert that exponential backoff is happening.
     $this->assertGreaterThanOrEqual($requestDelays[0], $requestDelays[1]);
     $this->assertGreaterThanOrEqual($requestDelays[1], $requestDelays[2]);
     $this->assertGreaterThanOrEqual($requestDelays[2], $requestDelays[3]);
+    $this->assertGreaterThanOrEqual($requestDelays[3], $requestDelays[4]);
+    $this->assertGreaterThanOrEqual($requestDelays[4], $requestDelays[5]);
+    $this->assertGreaterThanOrEqual($requestDelays[5], $requestDelays[6]);
+    $this->assertGreaterThanOrEqual($requestDelays[6], $requestDelays[7]);
+    $this->assertGreaterThanOrEqual($requestDelays[7], $requestDelays[8]);
+    $this->assertGreaterThanOrEqual($requestDelays[8], $requestDelays[9]);
 
     // Ensure jitter is being applied.
-    $this->assertNotEquals($baseWaits[0], $requestDelays[0]);
     $this->assertNotEquals($baseWaits[1], $requestDelays[1]);
     $this->assertNotEquals($baseWaits[2], $requestDelays[2]);
     $this->assertNotEquals($baseWaits[3], $requestDelays[3]);
+    $this->assertNotEquals($baseWaits[4], $requestDelays[4]);
+    $this->assertNotEquals($baseWaits[5], $requestDelays[5]);
+    $this->assertNotEquals($baseWaits[6], $requestDelays[6]);
+    $this->assertNotEquals($baseWaits[7], $requestDelays[7]);
+    $this->assertNotEquals($baseWaits[8], $requestDelays[8]);
+    $this->assertNotEquals($baseWaits[9], $requestDelays[9]);
+
+    // Ensure subsequent delay is never less than the minimum.
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[1]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[2]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[3]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[4]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[5]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[6]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[7]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[8]);
+    $this->assertGreaterThanOrEqual(HttpRequest::MIN_REQUEST_RETRY_DELAY, $requestDelays[9]);
+
+    // Ensure delay is never more than the maximum.
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[0]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[1]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[2]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[3]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[4]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[5]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[6]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[7]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[8]);
+    $this->assertLessThanOrEqual(HttpRequest::MAX_REQUEST_RETRY_DELAY, $requestDelays[9]);
+
+    // Ensure total delay sum is never more than 10s.
+    $this->assertLessThanOrEqual(10000, array_sum($requestDelays));
 });
 
 test('a request is tried 3 times before failing in the event of a 429', function(): void {
