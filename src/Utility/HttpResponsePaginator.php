49d6c5ec6e0815c33338da0999adb762878b00bb
Evan Sims <hello@evansims.com>
2021-09-10 11:34:35 -0400

    Revert "fix: Fix PHP 8.1 deprecation warnings"
    This reverts commit 88922ce300adf74933cb1384ab7295a128cafffb.

--- src/Utility/HttpResponsePaginator.php
+++ src/Utility/HttpResponsePaginator.php
@@ -154,7 +154,6 @@ final class HttpResponsePaginator implements \Countable, \Iterator
      *
      * @codeCoverageIgnore
      */
-    #[ReturnTypeWillChange]
     public function current()
     {
         if ($this->valid()) {
