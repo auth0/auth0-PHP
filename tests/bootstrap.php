5cb90328402800b3eb9a97ec21f6a6a9de908191
Evan Sims <hello@evansims.com>
2021-09-10 11:21:02 -0400

    tests: Fix ini_set warnings in 8.1(?)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,12 @@
 
 declare(strict_types=1);
 
+@ini_set('session.use_cookies', 'false');
+@ini_set('session.cache_limiter', 'false');
+
 define('AUTH0_TESTS_DIR', dirname(__FILE__));
 
 require_once join(DIRECTORY_SEPARATOR, [AUTH0_TESTS_DIR, '..', 'vendor', 'autoload.php']);
-
-ini_set('session.use_cookies', 'false');
-ini_set('session.cache_limiter', 'false');
 
 if (! defined('AUTH0_PHP_TEST_JSON_DIR')) {
     define('AUTH0_PHP_TEST_JSON_DIR', join(DIRECTORY_SEPARATOR, [AUTH0_TESTS_DIR, 'json']) . DIRECTORY_SEPARATOR);
