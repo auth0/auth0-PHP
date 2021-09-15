da58dec7f0243a6c370707d37a8c0eba642c49b5
Evan Sims <hello@evansims.com>
2021-09-15 12:46:19 -0400

    tests: Move `error_reporting`, `use_cookies` and `cache_limiter` vars into phpunit manifest

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,6 +10,6 @@ php ./vendor/bin/phpstan analyse --ansi --memory-limit 512M
 
 php ./vendor/bin/psalm
 
-php -d error_reporting='E_ALL ^ E_DEPRECATED' -d session.use_cookies=false -d session.cache_limiter=false ./vendor/bin/pest --stop-on-failure --coverage
+php ./vendor/bin/pest --stop-on-failure --coverage
 
 php --version
