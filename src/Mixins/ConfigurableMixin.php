aeccaa01541622b621e265ed9f63d34998d702ee
Evan Sims <evan.sims@okta.com>
2022-09-19 12:43:19 -0500

    [SDK-3633] Treat passing an empty string to SdkConfiguration as the default undefined value type of NULL

--- src/Mixins/ConfigurableMixin.php
+++ src/Mixins/ConfigurableMixin.php
@@ -264,6 +264,10 @@ trait ConfigurableMixin
             throw \Auth0\SDK\Exception\ConfigurationException::setIncompatible($propertyName, (string) $expectedType, $propertyType);
         }
 
+        if ($propertyType === 'string' && is_string($propertyValue) && strlen(trim($propertyValue)) === 0 && in_array('NULL', $allowedTypes, true)) {
+            $propertyValue = null;
+        }
+
         if (method_exists($this, 'onStateChange')) {
             $propertyValue = $this->onStateChange($propertyName, $propertyValue);
         }
