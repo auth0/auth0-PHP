de848bbf4da911ceccdbd9265b8f08c088d8a42f
Evan Sims <hello@evansims.com>
2023-01-17 20:16:59 -0600

    Update ConfigurableMixin.php

--- src/Mixins/ConfigurableMixin.php
+++ src/Mixins/ConfigurableMixin.php
@@ -115,7 +115,11 @@ trait ConfigurableMixin
         $filtered = [];
 
         foreach ($filtering as $i => $s) {
-            $s = trim($s);
+            if (! is_scalar($s)) {
+                continue;
+            }
+
+            $s = trim((string) $s);
 
             if ('' !== $s && ! \in_array($s, $filtered, true)) {
                 $filtered[$i] = $s;
@@ -134,7 +138,7 @@ trait ConfigurableMixin
     }
 
     /**
-     * @param  array<mixed>|null  $filtering  an array to filter, or NULL
+     * @param  array<mixed>|null  $filtering an array to filter, or NULL
      * @param  bool  $keepKeys  Optional. Whether to keep array keys or reindex the array appropriately.
      * @return array<mixed>|null
      */
@@ -151,7 +155,7 @@ trait ConfigurableMixin
                 $s = trim($s);
             }
 
-            if ('' !== $s && (\is_int($i) && ! \in_array($s, $filtered, true) || \is_string($i) && ! \array_key_exists($i, $filtered))) {
+            if ('' !== $s && null !== $s && ! \array_key_exists($i, $filtered)) {
                 $filtered[$i] = $s;
             }
         }
@@ -180,13 +184,11 @@ trait ConfigurableMixin
 
             $host = parse_url($domain, PHP_URL_HOST);
 
-            if (! \is_string($host)) {
+            // @codeCoverageIgnoreStart
+            if (! \is_string($host) || '' === $host) {
                 return null;
             }
-
-            if ('' === $host) {
-                return null;
-            }
+            // @codeCoverageIgnoreEnd
 
             $parts = explode('.', $host);
 
@@ -208,113 +210,14 @@ trait ConfigurableMixin
 
     private function filterString(string $str, bool $allowLineBreaks = false): string
     {
-        $filtered = $this->filterStringUtf8($str);
+        mb_regex_encoding('UTF-8');
 
-        if (str_contains($filtered, '<')) {
-            $filtered = preg_replace_callback('%<[^>]*?((?=<)|>|$)%', function ($matches) {
-                if (str_contains($matches[0], '>')) {
-                    return $this->filterHtml($matches[0]);
-                }
+        $processed = trim($str);
+        $processed = trim(\htmlspecialchars_decode($processed, \ENT_QUOTES | \ENT_SUBSTITUTE));
+        $processed = trim(\strip_tags($processed));
+        $processed = trim(\htmlspecialchars($processed, \ENT_QUOTES | \ENT_SUBSTITUTE, 'UTF-8', false));
+        $processed = mb_ereg_replace('[^\p{Ll}\p{Lu}\p{Nd}\p{Pd}\p{Pc}\p{Zs}\:\/\.\?\=]', '', $processed);
 
-                return $matches[0];
-            }, $filtered) ?? '';
-            $filtered = $this->filterStringTags($filtered, false);
-            $filtered = str_replace("<\n", "&lt;\n", $filtered);
-        }
-
-        if (! $allowLineBreaks) {
-            $filtered = preg_replace('/[\r\n\t ]+/', ' ', $filtered) ?? '';
-        }
-
-        $filtered = trim($filtered);
-        $found = false;
-
-        while (preg_match('/%[a-f0-9]{2}/i', $filtered, $match)) {
-            $filtered = str_replace($match[0], '', $filtered);
-            $found = true;
-        }
-
-        if ($found) {
-            $filtered = preg_replace('/ +/', ' ', $filtered);
-
-            if (null !== $filtered) {
-                $filtered = trim($filtered);
-            }
-        }
-
-        if (! \is_string($filtered)) {
-            return '';
-        }
-
-        return $filtered;
-    }
-
-    private function filterHtml(string $str, int $flags = ENT_QUOTES | ENT_SUBSTITUTE): string
-    {
-        if ('' === $str) {
-            return '';
-        }
-
-        if (false === preg_match('/[&<>"\']/', $str)) {
-            return $str;
-        }
-
-        return htmlspecialchars($str, $flags, 'UTF-8', false);
-    }
-
-    private function filterStringTags(string $str, bool $allowLineBreaks = false): string
-    {
-        $str = preg_replace('@<(script|style)[^>]*?>.*?</\\1>@si', '', $str);
-
-        if (null === $str) {
-            return '';
-        }
-
-        $str = strip_tags($str);
-
-        if (! $allowLineBreaks) {
-            $str = preg_replace('/[\r\n\t ]+/', ' ', $str);
-
-            if (null === $str) {
-                return '';
-            }
-        }
-
-        return trim($str);
-    }
-
-    private function filterStringUtf8(string $str, bool $strip = false): string
-    {
-        if ('' === $str) {
-            return '';
-        }
-
-        static $pcreSupported = null;
-
-        if (null === $pcreSupported) {
-            try {
-                $pcreSupported = preg_match('/^./u', 'a');
-            } catch (\Throwable $th) {
-                $pcreSupported = false;
-            }
-        }
-
-        if (! $pcreSupported) {
-            return $str;
-        }
-
-        if (1 === @preg_match('/^./us', $str)) {
-            return $str;
-        }
-
-        if ($strip && \function_exists('iconv')) {
-            $response = iconv('utf-8', 'utf-8', $str);
-
-            if (\is_string($response)) {
-                return $response;
-            }
-        }
-
-        return '';
+        return (is_string($processed) && '' !== $processed) ? $processed : '';
     }
 }
