57642895f8b7dc6ef9dd5ddee2fc047dfcf7fdcb
Evan Sims <evan.sims@okta.com>
2023-01-24 10:32:32 -0600

    feat(Management): Add support for client credentials endpoints (#700)

--- src/Contract/API/Management/ClientsInterface.php
+++ src/Contract/API/Management/ClientsInterface.php
@@ -105,4 +105,75 @@ interface ClientsInterface
         string $id,
         ?RequestOptions $options = null,
     ): ResponseInterface;
+
+    /**
+     * Add new credentials to the specified Client.
+     *
+     * @param  string  $clientId Client to add credentials to
+     * @param  array<mixed>  $body  Additional body content to pass with the API request. See @see for supported options.
+     * @param  RequestOptions|null  $options  Optional. Additional request options to use, such as a field filtering or pagination. (Not all endpoints support these. See @see for supported options.)
+     *
+     * @throws \Auth0\SDK\Exception\NetworkException when the API request fails due to a network error
+     *
+     * @see https://auth0.com/docs/api/management/v2#!/Client_Credentials/post_client_credentials
+     */
+    public function createCredentials(
+        string $clientId,
+        array $body,
+        ?RequestOptions $options = null,
+    ): ResponseInterface;
+
+    /**
+     * Get all credentials from the specified Client.
+     *
+     * @param  string  $clientId  Client to retrieve credentials for
+     * @param  array<int|string|null>|null  $parameters  Optional. Additional query parameters to pass with the API request. See @see for supported options.
+     * @param  RequestOptions|null  $options  Optional. Additional request options to use, such as a field filtering or pagination. (Not all endpoints support these. See @see for supported options.)
+     *
+     * @throws \Auth0\SDK\Exception\ArgumentException when an invalid `clientId` is provided
+     * @throws \Auth0\SDK\Exception\NetworkException when the API request fails due to a network error
+     *
+     * @see https://auth0.com/docs/api/management/v2#!/Client_Credentials/get_client_credentials
+     */
+    public function getCredentials(
+        string $clientId,
+        ?array $parameters = null,
+        ?RequestOptions $options = null,
+    ): ResponseInterface;
+
+    /**
+     * Get a specific credential from the specified Client.
+     *
+     * @param  string  $clientId  Client to retrieve credential for
+     * @param  string  $credentialId  credential to retrieve
+     * @param  RequestOptions|null  $options  Optional. Additional request options to use, such as a field filtering or pagination. (Not all endpoints support these. See @see for supported options.)
+     *
+     * @throws \Auth0\SDK\Exception\ArgumentException when an invalid `clientId` or `credentialId` are provided
+     * @throws \Auth0\SDK\Exception\NetworkException when the API request fails due to a network error
+     *
+     * @see https://auth0.com/docs/api/management/v2#!/Client_Credentials/get_client_credentials_by_id
+     */
+    public function getCredential(
+        string $clientId,
+        string $credentialId,
+        ?RequestOptions $options = null,
+    ): ResponseInterface;
+
+    /**
+     * Delete a credential from the specified Client.
+     *
+     * @param  string  $clientId  client to delete credential for
+     * @param  string  $credentialId  credential to delete
+     * @param  RequestOptions|null  $options  Optional. Additional request options to use, such as a field filtering or pagination. (Not all endpoints support these. See @see for supported options.)
+     *
+     * @throws \Auth0\SDK\Exception\ArgumentException when an invalid `clientId` or `credentialId` are provided
+     * @throws \Auth0\SDK\Exception\NetworkException when the API request fails due to a network error
+     *
+     * @see https://auth0.com/docs/api/management/v2#!/Client_Credentials/delete_client_credentials_by_id
+     */
+    public function deleteCredential(
+        string $clientId,
+        string $credentialId,
+        ?RequestOptions $options = null,
+    ): ResponseInterface;
 }
