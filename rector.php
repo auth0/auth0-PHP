231904dbd8e78070af92ffd962ece8625560f756
Evan Sims <hello@evansims.com>
2023-03-13 10:16:35 -0500

    Adjust tests for upstream dev dependency changes

--- rector.php
+++ rector.php
@@ -2,29 +2,21 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
 
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
     $rectorConfig->sets([
         SetList::CODE_QUALITY,
     ]);
 
-    $rectorConfig->skip([
-        CompleteDynamicPropertiesRector::class => [
-            // Breaks PEST
-            __DIR__ . '/tests/Utilities/MockApi.php',
-        ],
-    ]);
-
-    $rectorConfig->rule(TypedPropertyRector::class);
-
-    $rectorConfig->phpVersion(PhpVersion::PHP_80);
-
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
 };
