726e75d571ae799b879f34f3984f421649b3c5fc
Evan Sims <hello@evansims.com>
2023-03-16 00:27:08 -0500

    Update tests

--- tests/Unit/Utility/HttpResponseTest.php
+++ tests/Unit/Utility/HttpResponseTest.php
@@ -3,16 +3,53 @@
 declare(strict_types=1);
 
 use Auth0\SDK\Utility\HttpResponse;
+use Auth0\Tests\Utilities\HttpResponseGenerator;
 use Psr\Http\Message\ResponseInterface;
 
 uses()->group('utility', 'utility.http_response', 'networking');
 
 test('getHeaders() returns the expected response', function(): void {
-    $response = Mockery::mock(ResponseInterface::class);
-    $response->shouldReceive('getStatusCode')->andReturn(200);
-    $response->shouldReceive('getHeaders')->andReturn(['X-TEST' => 'Testing']);
-    $response->shouldReceive('getBody')->andReturn('');
+    $headers = ['X-TEST' => uniqid()];
+    $body = json_encode(['Hello World' => uniqid()]);
 
-    expect(HttpResponse::getHeaders($response))
-        ->toEqualCanonicalizing(['X-TEST' => 'Testing']);
+    $response = HttpResponseGenerator::create(
+        $body,
+        201,
+        $headers
+    );
+
+    $headers = HttpResponse::getHeaders($response);
+    $body = HttpResponse::getContent($response);
+    $status = HttpResponse::getStatusCode($response);
+    $success = HttpResponse::wasSuccessful($response, 201);
+
+    expect($success)
+        ->toBeTrue();
+
+    expect($status)
+        ->toEqual(201);
+
+    expect($body)
+        ->toEqual($body);
+
+    expect($headers)
+        ->toHaveKeys(['X-TEST'])
+        ->each->toEqual($headers['X-TEST']);
+});
+
+test('getContent() returns the expected response', function(): void {
+    $response = $this->createMock(ResponseInterface::class);
+    $response->method('getBody')->willReturn(123);
+
+    expect(HttpResponse::getContent($response))
+        ->toEqual('123');
+});
+
+test('decodeContent() returns the expected response', function(): void {
+    $response = $this->createMock(ResponseInterface::class);
+    $response->method('getBody')->willReturn(json_encode(['Hello World' => uniqid()]));
+
+    expect(HttpResponse::decodeContent($response))
+        ->toBeArray()
+        ->toHaveKey('Hello World');
 });
