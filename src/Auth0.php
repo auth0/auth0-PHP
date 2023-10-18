ef7634a50857598de44c04692bdc9b23ad201e10
Evan Sims <evan.sims@okta.com>
2023-10-18 17:54:14 -0500

    Release 8.8.0 (#735)
    ### Added
    
    - Support `initiate_login_uri` property for `PATCH` requests to the
    `/api/v2/clients/:id` Management API endpoint.
    [#732](https://github.com/auth0/auth0-PHP/pull/732)
    
    ### Fixed
    
    - Token generator typo in claim header.
    [#729](https://github.com/auth0/auth0-PHP/pull/729)
    
    ### Changed
    
    - `composer.json` now identifies `mbstring` as a required extension.
    [#730](https://github.com/auth0/auth0-PHP/pull/730)
    - PHP 8.3 nightlies have been added to the continuous integration
    testing matrix. [#733](https://github.com/auth0/auth0-PHP/pull/733)

--- src/Auth0.php
+++ src/Auth0.php
@@ -21,7 +21,7 @@ final class Auth0 implements Auth0Interface
     /**
      * @var string
      */
-    public const VERSION = '8.7.1';
+    public const VERSION = '8.8.0';
 
     /**
      * Authentication Client.
