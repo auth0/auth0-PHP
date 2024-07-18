0b5b366ee43665dbb30db87fb6b3c2ea4fc41193
Simon Rogers <webdev@midnite.uk>
2024-07-18 12:46:11 +0100

    Revert formatting

--- tests/Unit/Utility/HttpClientTest.php
+++ tests/Unit/Utility/HttpClientTest.php
@@ -11,7 +11,7 @@ use Auth0\Tests\Utilities\MockDomain;
 
 uses()->group('utility', 'utility.http_client', 'networking');
 
-beforeEach(function (): void {
+beforeEach(function(): void {
     $this->config = new SdkConfiguration([
         'domain' => MockDomain::valid(),
         'cookieSecret' => uniqid(),
@@ -22,14 +22,13 @@ beforeEach(function (): void {
     $this->client = new HttpClient($this->config, HttpClient::CONTEXT_MANAGEMENT_CLIENT);
 
     $this->httpResponse200 = HttpResponseGenerator::create('{"status": "ok"}', 200);
-    $this->httpResponse429 = HttpResponseGenerator::create('{"error": "too_many_requests", "error_description": "Rate limit exceeded"}',
-        429);
+    $this->httpResponse429 = HttpResponseGenerator::create('{"error": "too_many_requests", "error_description": "Rate limit exceeded"}', 429);
 });
 
-test('a 429 response is not retried if httpMaxRetries is zero', function (): void {
+test('a 429 response is not retried if httpMaxRetries is zero', function(): void {
     $this->config->setHttpMaxRetries(0);
 
-    for ($i = 0; $i < 3; $i++) {
+    for ($i=0; $i < 3; $i++) {
         $this->client->mockResponse(clone $this->httpResponse429);
     }
 
@@ -44,15 +43,15 @@ test('a 429 response is not retried if httpMaxRetries is zero', function (): voi
     expect($requestCount)->toEqual(1);
 });
 
-test('a 429 response is not retried more than the hard cap', function (): void {
+test('a 429 response is not retried more than the hard cap', function(): void {
     $this->config->setHttpMaxRetries(HttpRequest::MAX_REQUEST_RETRIES * 2);
 
-    for ($i = 0; $i < 11; $i++) {
+    for ($i=0; $i < 11; $i++) {
         $this->client->mockResponse(clone $this->httpResponse429);
     }
 
     $response = $this->client->method('get')
-        ->addPath(['client'])
+    ->addPath(['client'])
         ->call();
 
     expect(HttpResponse::getStatusCode($response))->toEqual(429);
@@ -62,21 +61,21 @@ test('a 429 response is not retried more than the hard cap', function (): void {
     expect($requestCount)->toEqual(10);
 });
 
-test('an exponential back-off and jitter are being applied', function (): void {
+test('an exponential back-off and jitter are being applied', function(): void {
     $this->config->setHttpMaxRetries(10);
 
     $baseWaits = [0];
     $baseWaitSum = 0;
 
-    for ($i = 0; $i < 10; $i++) {
+    for ($i=0; $i < 10; $i++) {
         $this->client->mockResponse(clone $this->httpResponse429);
-        $baseWait = (int)(100 * pow(2, $i));
+        $baseWait = (int) (100 * pow(2, $i));
         $baseWaits[] = $baseWait;
         $baseWaitSum += $baseWait;
     }
 
     $response = $this->client->method('get')
-        ->addPath(['client'])
+    ->addPath(['client'])
         ->call();
 
     $requestCount = $this->client->getLastRequest()->getRequestCount();
@@ -135,15 +134,15 @@ test('an exponential back-off and jitter are being applied', function (): void {
     expect(array_sum($requestDelays))->toBeLessThanOrEqual(10000);
 });
 
-test('a request is tried 3 times before failing in the event of a 429', function (): void {
-    for ($i = 0; $i < 3; $i++) {
+test('a request is tried 3 times before failing in the event of a 429', function(): void {
+    for ($i=0; $i < 3; $i++) {
         $this->client->mockResponse(clone $this->httpResponse429);
     }
 
     $this->client->mockResponse(clone $this->httpResponse200);
 
     $response = $this->client->method('get')
-        ->addPath(['client'])
+    ->addPath(['client'])
         ->call();
 
     expect(HttpResponse::getStatusCode($response))->toEqual(429);
@@ -153,7 +152,7 @@ test('a request is tried 3 times before failing in the event of a 429', function
     expect($requestCount)->toEqual(3);
 });
 
-test('a request recovers from a 429 response and returns the successful result', function (): void {
+test('a request recovers from a 429 response and returns the successful result', function(): void {
     $this->client->mockResponses([
         clone $this->httpResponse429,
         clone $this->httpResponse200,
@@ -161,7 +160,7 @@ test('a request recovers from a 429 response and returns the successful result',
     ]);
 
     $response = $this->client->method('get')
-        ->addPath(['client'])
+    ->addPath(['client'])
         ->call();
 
     expect(HttpResponse::getStatusCode($response))->toEqual(200);
@@ -172,7 +171,7 @@ test('a request recovers from a 429 response and returns the successful result',
 });
 
 
-test('the callback of a mock response is called', function (): void {
+test('the callback of a mock response is called', function(): void {
     $count = 0;
 
     $this->client->mockResponse(
