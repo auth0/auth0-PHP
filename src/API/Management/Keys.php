e1c552e811ceef34fd477740896d4c0f2b416c6f
Snehil Kishore <snehil.kishore@okta.com>
2024-11-13 13:07:32 +0530

    Adding Support For BYOK

--- src/API/Management/Keys.php
+++ src/API/Management/Keys.php
@@ -6,6 +6,7 @@ namespace Auth0\SDK\API\Management;
 
 use Auth0\SDK\Contract\API\Management\KeysInterface;
 use Auth0\SDK\Utility\Request\RequestOptions;
+use Auth0\SDK\Utility\Toolkit;
 use Psr\Http\Message\ResponseInterface;
 
 /**
@@ -15,12 +16,119 @@ use Psr\Http\Message\ResponseInterface;
  */
 final class Keys extends ManagementEndpoint implements KeysInterface
 {
+    public function deleteEncryptionKey(
+        string $kId,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$kId] = Toolkit::filter([$kId])->string()->trim();
+
+        Toolkit::assert([
+            [$kId, \Auth0\SDK\Exception\ArgumentException::missing('kId')],
+        ])->isString();
+
+        return $this->getHttpClient()
+            ->method('delete')->addPath(['keys', 'encryption', $kId])
+            ->withOptions($options)
+            ->call();
+    }
+
+    public function getEncryptionKey(
+        string $kId,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$kId] = Toolkit::filter([$kId])->string()->trim();
+
+        Toolkit::assert([
+            [$kId, \Auth0\SDK\Exception\ArgumentException::missing('kId')],
+        ])->isString();
+
+        return $this->getHttpClient()
+            ->method('get')
+            ->addPath(['keys', 'encryption', $kId])
+            ->withOptions($options)
+            ->call();
+    }
+
+    public function getEncryptionKeys(
+        ?array $parameters = null,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$parameters] = Toolkit::filter([$parameters])->array()->trim();
+
+        /** @var array<null|int|string> $parameters */
+
+        return $this->getHttpClient()
+            ->method('get')
+            ->addPath(['keys', 'encryption'])
+            ->withParams($parameters)
+            ->withOptions($options)
+            ->call();
+    }
+
+    public function postEncryption(
+        array $body,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$body] = Toolkit::filter([$body])->array()->trim();
+
+        Toolkit::assert([
+            [$body, \Auth0\SDK\Exception\ArgumentException::missing('body')],
+        ])->isArray();
+
+        return $this->getHttpClient()
+            ->method('post')
+            ->addPath(['keys', 'encryption'])
+            ->withBody((object) $body)
+            ->withOptions($options)
+            ->call();
+    }
+
+    public function postEncryptionKey(
+        string $kId,
+        array $body,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$kId] = Toolkit::filter([$kId])->string()->trim();
+        [$body] = Toolkit::filter([$body])->array()->trim();
+
+        Toolkit::assert([
+            [$kId, \Auth0\SDK\Exception\ArgumentException::missing('kId')],
+        ])->isString();
+        Toolkit::assert([
+            [$body, \Auth0\SDK\Exception\ArgumentException::missing('body')],
+        ])->isArray();
+
+        return $this->getHttpClient()
+            ->method('post')
+            ->addPath(['keys', 'encryption', $kId])
+            ->withBody((object) $body)
+            ->withOptions($options)
+            ->call();
+    }
+
     public function postEncryptionRekey(
         ?RequestOptions $options = null,
     ): ResponseInterface {
         return $this->getHttpClient()
             ->method('post')
             ->addPath(['keys', 'encryption', 'rekey'])
+            ->withOptions($options)
+            ->call();
+    }
+
+    public function postEncryptionWrappingKey(
+        string $kId,
+        ?RequestOptions $options = null,
+    ): ResponseInterface {
+        [$kId] = Toolkit::filter([$kId])->string()->trim();
+
+        Toolkit::assert([
+            [$kId, \Auth0\SDK\Exception\ArgumentException::missing('kId')],
+        ])->isString();
+
+        return $this->getHttpClient()
+            ->method('post')
+            ->addPath(['keys', 'encryption', $kId, 'wrapping-key'])
             ->withOptions($options)
             ->call();
     }
