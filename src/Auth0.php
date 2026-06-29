6a656658a4d8e0a7d5a872c3b228f50b73c73997
Snehil Kishore <snehil.kishore@okta.com>
2026-06-29 13:50:03 +0530

    Release 9.0.0-beta.3

--- src/Auth0.php
+++ src/Auth0.php
@@ -22,7 +22,7 @@ final class Auth0 implements Auth0Interface
     /**
      * @var string
      */
-    public const VERSION = '9.0.0-beta.2';
+    public const VERSION = '9.0.0-beta.3';
 
     /**
      * Authentication Client.
