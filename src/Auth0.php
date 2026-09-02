ea164c2939cecdad59b2ef71ef865c5553377662
Snehil Kishore <snehil.kishore@okta.com>
2026-09-02 20:31:34 +0530

    Release 9.2.0 (#864)
    **Added**
    
    - feat: add Organization Templates, Connection Profile SCIM
    provisioning, and Network ACL HTTP message signatures
    [\#863](https://github.com/auth0/auth0-PHP/pull/863)
    ([fern-api[bot]](https://github.com/apps/fern-api))
    
    **Fixed**
    
    - fix: enforce back-channel logout on subsequent requests by rehydrating
    session state [\#852](https://github.com/auth0/auth0-PHP/pull/852)
    ([kishore7snehil](https://github.com/kishore7snehil))
    - fix: clear all session entries in SessionStore::purge() regardless of
    key order [\#851](https://github.com/auth0/auth0-PHP/pull/851)
    ([kishore7snehil](https://github.com/kishore7snehil))

--- src/Auth0.php
+++ src/Auth0.php
@@ -23,7 +23,7 @@ final class Auth0 implements Auth0Interface
     /**
      * @var string
      */
-    public const VERSION = '9.1.0';
+    public const VERSION = '9.2.0';
 
     /**
      * Authentication Client.
